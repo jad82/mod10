d5cebae68a1200777d8b42ef939f4e5e91eca7ad
jad82 <jan.drzymala@live.com>
2017-04-16 00:51:45 +0200

    comparing phones - index and scripts

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+function Telefon(marka, cena, kolor, pamięć, bateria) { 
+this.marka = marka; //dzięki użyciu this, właściwość "marka" obiektu, który stworzymy, przyjmie wartość argumentu marka 
+this.cena = cena; 
+this.kolor = kolor;
+this.pamięć = pamięć;
+this.bateria = bateria;
+};
+Telefon.prototype.printInfo = function() { 
+	console.log("Marka telefonu to " + this.marka + ", kolor to " + this.kolor + ", a cena to " + this.cena + ", pamięci RAM ma " + this.pamięć + ", bateria wytrzymuje " + this.bateria + " godzin czuwania." ); 
+};
+var iPhone6S = new Telefon("Apple", 2250, "srebrny", "2GB", 740);
+var GalaxyS6 = new Telefon("Samsung", 1797, "czarny", "4GB", "brak danych");
+var One  = new Telefon("OnePlus", 1299, "czerwony", "3GB", "brak danych");
+iPhone6S.printInfo();
+GalaxyS6.printInfo();
+One.printInfo(); 
